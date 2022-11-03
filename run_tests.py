67ba6c5abea77c26cd7ad7f2ad48c2909c91910a
rolysr <rolsanchez@yandex.com>
2022-11-03 03:05:32 -0400

    Vector feedback test passed

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,10 @@ from testing.query_expansion_test import *
 from testing.boolean_model_test import *
 from testing.vector_space_model_test import *
 
+from testing.vector_feedback_test import *
+
 # run_preprocessing_test()
 # run_query_expansion_test()
 # run_boolean_model_test()
-run_vector_space_model_test()
+# run_vector_space_model_test()
+# run_vector_feedback_test()
--- testing/query_expansion_test.py
+++ testing/query_expansion_test.py
@@ -3,4 +3,4 @@ from utils.query_expansion import *
 def run_query_expansion_test():
     query = "Hi my name is Bill"
 
-    print(query_expansion_by_synonyms(query))
+    print(query_expansion_by_synonyms(query)) 
