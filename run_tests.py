3e997821901ae8ae9c0fca7324ab89c88bc11ec4
rolysr <rolsanchez@yandex.com>
2022-11-04 21:10:05 -0400

    Basic qt frontend added without functionality

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
-from testing.boolean_model_test import run_boolean_model_test
+from testing.boolean_model_test import *
 from testing.preprocessing_test import *
 from testing.query_expansion_test import *
-from testing.boolean_model_test import *
+from testing.generalized_vector_space_model_test import *
 from testing.vector_space_model_test import *
 
 from testing.vector_feedback_test import *
