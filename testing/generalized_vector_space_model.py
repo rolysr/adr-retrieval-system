93eb3128ca2953f0458f1b7ae1f1fbf4221d3ade
aXoXoR2 <102486333+aXoXoR2@users.noreply.github.com>
2022-11-05 11:04:08 -0400

    Changes in test

---
+++ testing/generalized_vector_space_model.py
@@ -0,0 +1,23 @@
+from retrieval_models.generalized_vector_space_model.generalized_vector_space_model import *
+from utils.preprocessor import *
+
+def run_vector_space_model_test():
+    # Init preprocessor
+    preprocessor = Preprocessor('./datasets')
+
+    # Get all corpus
+    corpus = preprocessor.generate_preprocessed_documents()
+
+    # Init boolean model
+    vsm = GeneralizedVectorSpaceModel(corpus)
+
+    # Make a query for a document
+    doc = corpus[0].text
+    query = "An experimental study of a wing in a propeller slipstream"
+
+    # Parse query and document
+    d = vsm.parse_document(doc)
+    q = vsm.parse_query(query)
+
+    s = vsm.sim(d, q)
+    print(s)
